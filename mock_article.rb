7b9526229162f38fa6046f5ec27e1d55a88fd49c
Krzysztof <KrzysztofKolek91@gmail.com>
2014-11-13 00:37:43 +0100

    [#6] Use sample method.

--- mock_article.rb
+++ mock_article.rb
@@ -1,4 +1,4 @@
-load 'article_manager.rb'
+load 'article.rb'
 
 class MockArticle < Article
   @@WORD_LIST = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.'.split(%r{\W}).uniq
@@ -11,24 +11,20 @@ class MockArticle < Article
   end
   
   def generate_author
-    "#{ @@WORD_LIST[Random.rand(@@LENGTH-1)].capitalize } #{ @@WORD_LIST[Random.rand(@@LENGTH-1)] }".capitalize
+    @@WORD_LIST.sample(2).map{ |word| word.capitalize }.join(' ')
   end
   
   def generate_title
-    title = ''
-    Random.rand(1..5).times { title += "#{@@WORD_LIST[Random.rand(@@LENGTH-1)]} " }
-    title.capitalize.rstrip
+    @@WORD_LIST.sample(Random.rand(1..5)).join(' ').capitalize
   end
   
   def generate_sentence
-    sentence = ''
-    Random.rand(5..10).times { sentence += "#{@@WORD_LIST[Random.rand(@@LENGTH-1)]} " }
-    sentence.capitalize.rstrip + '.'
+    @@WORD_LIST.sample(Random.rand(5..10)).join(' ').capitalize + '.'
   end
 
   def generate_content
     content = ''
-    Random.rand(1..5).times { content += "#{self.generate_sentence} " }
+    Random.rand(1..5).times { content += "#{ self.generate_sentence } " }
     content.rstrip
   end
   
