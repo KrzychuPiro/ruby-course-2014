2d05560302fae8c54e3f305301d8937a16555c8f
Krzysztof <KrzysztofKolek91@gmail.com>
2014-11-13 02:35:16 +0100

    [#6] Corrected split RegEx in Word_List.

--- mock_article.rb
+++ mock_article.rb
@@ -1,7 +1,7 @@
 load 'article.rb'
 
 class MockArticle < Article
-  @@WORD_LIST = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.'.split(%r{\W}).uniq
+  @@WORD_LIST = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.'.split(%r{[^\w]+})
   @@LENGTH = @@WORD_LIST.length
 
   def initialize
@@ -19,7 +19,7 @@ class MockArticle < Article
   end
   
   def generate_sentence
-    @@WORD_LIST.sample(Random.rand(5..10)).join(' ').capitalize + '.'
+    (@@WORD_LIST.sample(Random.rand(5..10)).join(' ').capitalize + '.')
   end
 
   def generate_content
