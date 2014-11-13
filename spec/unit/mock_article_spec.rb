97e34e8e4273b54610bfc9ac078a2abf7cbf1b67
Krzysztof <KrzysztofKolek91@gmail.com>
2014-11-13 02:52:51 +0100

    [#11] Tests for MockArticle class.

---
+++ spec/unit/mock_article_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../../mock_article'
+
+class TestMockArticle < Test::Unit::TestCase
+
+  def setup
+    @mock_article = MockArticle.new
+  end
+
+  def test_simple
+    assert_match(%r{[A-Z]\w* [A-Z]\w*}, @mock_article.generate_author)
+    assert_match(%r{[A-Z]\w*( \w+){0,4}}, @mock_article.generate_title)
+    assert_match(%r{[A-Z]\w*( \w+){4,9}\.}, @mock_article.generate_sentence)
+    assert_match(%r{([A-Z]\w*( \w+){4,9}\.)( \1){0,4}}, @mock_article.generate_content)
+    assert_operator(100, :>=, @mock_article.random_number)
+    assert_operator(0, :<, @mock_article.random_number)
+  end
+end
