00b28869309038aaa79449812385c307186e2c68
Krzysztof <KrzysztofKolek91@gmail.com>
2014-12-04 04:32:49 +0100

    [#11] Add unique test methods.

--- spec/unit/mock_article_spec.rb
+++ spec/unit/mock_article_spec.rb
@@ -6,11 +6,23 @@ class TestMockArticle < Test::Unit::TestCase
     @mock_article = MockArticle.new
   end
 
-  def test_simple
+  def test_author
     assert_match(%r{[A-Z]\w* [A-Z]\w*}, @mock_article.generate_author)
+  end
+
+  def test_title
     assert_match(%r{[A-Z]\w*( \w+){0,4}}, @mock_article.generate_title)
+  end
+
+  def test_sentence
     assert_match(%r{[A-Z]\w*( \w+){4,9}\.}, @mock_article.generate_sentence)
+  end
+
+  def test_content
     assert_match(%r{([A-Z]\w*( \w+){4,9}\.)( \1){0,4}}, @mock_article.generate_content)
+  end
+
+  def test_random
     assert_operator(100, :>=, @mock_article.random_number)
     assert_operator(0, :<, @mock_article.random_number)
   end
