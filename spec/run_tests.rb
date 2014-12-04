f098b8b39547a519182bf644dc313fb0b49e051b
Krzysztof <KrzysztofKolek91@gmail.com>
2014-12-04 04:10:20 +0100

    [#8] Corrected files loading.

--- spec/run_tests.rb
+++ spec/run_tests.rb
@@ -1,4 +1,3 @@
 require 'test/unit'
-require_relative 'unit/article_spec'
-require_relative 'unit/article_manager_spec.rb'
-require_relative 'unit/mock_article_spec.rb'
+Dir.foreach('spec/unit') { |file| require_relative "unit/#{file}" if %r{(\w)+_spec\.rb}.match(file) }
+
